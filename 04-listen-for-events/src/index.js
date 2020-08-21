6950239f828e4a6b565810f198d326d69f1b3d5d
danielacb <dgdanielabarbosa@gmail.com>
2020-08-20 22:37:05 -0300

    code challenge 04

--- 04-listen-for-events/src/index.js
+++ 04-listen-for-events/src/index.js
@@ -1,14 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import ScotchInfoBar from './ScotchInfoBar';
 import './styles.css';
 
 function App() {
+  const [phrase, setPhrase] = useState('');
+
+  if (phrase === 'open sesame') {
+    alert('there you are');
+  }
+
   return (
     <div className="App">
       <h2>What's the secret phrase?</h2>
 
-      <input type="text" placeholder="Super duper secret" />
+      <input type="text" placeholder="Super duper secret" value={phrase} onChange={e => setPhrase(e.target.value)} />
 
       <p>
         Hint: It's <strong>open sesame</strong>
