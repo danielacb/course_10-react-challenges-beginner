d6333f0a0a7c8b4fe7305a49c7c46e7824328475
danielacb <dgdanielabarbosa@gmail.com>
2020-08-22 21:32:35 -0300

    code challenge 05

--- 05-calculator-adder/src/index.js
+++ 05-calculator-adder/src/index.js
@@ -1,21 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import ScotchInfoBar from './ScotchInfoBar';
 import './styles.css';
 
 function App() {
+  const [number1, setnumber1] = useState(0);
+  const [number2, setnumber2] = useState(0);
+  const [sum, setSum] = useState(0);
+
   return (
     <div className="App">
       <h1>Adding Two Numbers</h1>
 
       <div className="number-inputs">
-        <input type="number" placeholder="0" />
-        <input type="number" placeholder="0" />
+        <input type="number" placeholder="0" onChange={(e) => setnumber1(+e.target.value)} />
+        <input type="number" placeholder="0" onChange={(e) => setnumber2(+e.target.value)} />
       </div>
 
-      <button>Add Them!</button>
+      <button onClick={() => setSum(number1 + number2)}>Add Them!</button>
 
-      <h2>0</h2>
+      <h2>{sum}</h2>
 
       <ScotchInfoBar seriesNumber={5} />
     </div>
